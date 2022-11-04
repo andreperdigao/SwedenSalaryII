cd0d1f415685b6cbae3f67f2fe76ca27ebff7923
andreperdigao <andre_perdigao@hotmail.com>
2022-11-04 14:04:40 +0100

    Add binding

--- app/src/main/java/andre/perdigao/cursoandroid/swedensalaryrefactored/MainActivity.kt
+++ app/src/main/java/andre/perdigao/cursoandroid/swedensalaryrefactored/MainActivity.kt
@@ -1,24 +1,32 @@
 package andre.perdigao.cursoandroid.swedensalaryrefactored
 
 import andre.perdigao.cursoandroid.swedensalaryrefactored.databinding.ActivityMainBinding
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
-
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
 
     private var salary = 0
+    private var apartment = 0
+    private var internet = 0
+    private var electricity = 0
+    private var food = 0
+    private var transport = 0
+    private var insurance = 0
     private var net = 0
-
+    private var cost = 0
+    private var rest = 0
     private val tax = 0.7618
 
+    private val keyNet = "net"
+    private val keyCost = "cost"
+    private val keyRest = "rest"
 
-    private var salaryValue : TextView? = null
-    private var netSalaryValue : TextView? = null
-    private var btnCalculateVal : Button? = null
 
     private lateinit var binding : ActivityMainBinding
 
@@ -28,13 +36,9 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
-        salaryValue = binding.editSalaryValue
-
-        netSalaryValue = binding.txtNetSalaryValue
-
-        btnCalculateVal = binding.btnCalculate
-
         calculate()
+
+        clear()
 
 
     }
@@ -42,13 +46,146 @@ class MainActivity : AppCompatActivity() {
     private fun calculate() {
 
         binding.btnCalculate.setOnClickListener {
-            salary = salaryValue!!.text.toString().toInt()
-            net =(salary*tax).toInt()
-            netSalaryValue!!.text = net.toString()
+
+            if (checkValues()) {
+
+                Toast.makeText(
+                    this, "Calculating...",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                salary = binding.editSalaryValue.text.toString().toInt()
+                apartment = binding.editRentValue.text.toString().toInt()
+                electricity = binding.editElectricityValue.text.toString().toInt()
+                internet = binding.editInternetValue.text.toString().toInt()
+                food = binding.editFoodValue.text.toString().toInt()
+                transport = binding.editTransportValue.text.toString().toInt()
+                insurance = binding.editInsuranceValue.text.toString().toInt()
+
+                net = (salary * tax).toInt()
+                cost = apartment + electricity + internet + food + transport + insurance
+                rest = net - cost
+
+
+                binding.txtNetSalaryValue.text = net.toString()
+                binding.txtLifeCostValue.text = cost.toString()
+                binding.txtRestValue.text = rest.toString()
+            }
+
+            closeKeyboard(binding.txtRestValue)
 
         }
 
 
+    }
+
+    private fun clear(){
+
+        binding.btnClear.setOnClickListener {
+
+            Toast.makeText(
+                this, "Clear Success...",
+                Toast.LENGTH_SHORT
+            ).show()
+
+
+            net = 0
+            cost = 0
+            rest = 0
+            binding.txtNetSalaryValue.text = ""
+            binding.txtLifeCostValue.text = ""
+            binding.txtRestValue.text = ""
+            binding.editSalaryValue.text.clear()
+            binding.editRentValue.text.clear()
+            binding.editElectricityValue.text.clear()
+            binding.editFoodValue.text.clear()
+            binding.editInternetValue.text.clear()
+            binding.editInsuranceValue.text.clear()
+            binding.editTransportValue.text.clear()
+
+        }
+
+    }
+
+    private fun checkValues() : Boolean {
+
+        var checkValues = true
+
+        if (binding.editSalaryValue.text.toString().isEmpty()) {
+            binding.editSalaryValue.error = "Please insert you Salary"
+            binding.editSalaryValue.requestFocus()
+            checkValues = false
+        }
+
+        else if (binding.editRentValue.text.toString().isEmpty()) {
+            binding.editRentValue.error = "Please insert Rent Cost"
+            binding.editRentValue.requestFocus()
+            checkValues = false
+        }
+
+        else if (binding.editElectricityValue.text.toString().isEmpty()) {
+            binding.editElectricityValue.error = "Please insert Electricity Cost"
+            binding.editElectricityValue.requestFocus()
+            checkValues = false
+        }
+
+        else if (binding.editInternetValue.text.toString().isEmpty()) {
+            binding.editInternetValue.error = "Please insert Broadband Cost"
+            binding.editInternetValue.requestFocus()
+            checkValues = false
+        }
+
+        else if (binding.editFoodValue.text.toString().isEmpty()) {
+            binding.editFoodValue.error = "Please insert Food/Household Cost"
+            binding.editFoodValue.requestFocus()
+            checkValues = false
+        }
+
+        else if (binding.editTransportValue.text.toString().isEmpty()) {
+            binding.editTransportValue.error = "Please insert Transport Cost"
+            binding.editTransportValue.requestFocus()
+            checkValues = false
+        }
+
+        else if (binding.editInsuranceValue.text.toString().isEmpty()) {
+            binding.editInsuranceValue.error = "Please insert Insurance Cost"
+            binding.editInsuranceValue.requestFocus()
+            checkValues = false
+        }
+
+        return checkValues
+
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(keyNet,net)
+        outState.putInt(keyCost,cost)
+        outState.putInt(keyRest,rest)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        net = savedInstanceState.getInt(keyNet,0)
+        if (net!= 0) {
+            binding.txtNetSalaryValue.text = net.toString()
+        }
+        cost = savedInstanceState.getInt(keyCost,0)
+        if (cost != 0) {
+            binding.txtLifeCostValue.text = cost.toString()
+        }
+        rest = savedInstanceState.getInt(keyRest,0)
+        if (rest != 0) {
+            binding.txtRestValue.text = rest.toString()
+        }
+
+    }
+
+    private fun closeKeyboard(view: View){
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken,0)
     }
 
 
